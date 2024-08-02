7ac7e1cbb71c17f69e48bbc41bfe5d9224cdee2d
juno0432 <129417004+juno0432@users.noreply.github.com>
2024-08-02 17:10:26 +0900

    수정

--- src/control/ScoreControl.java
+++ src/control/ScoreControl.java
@@ -14,9 +14,7 @@ public class ScoreControl
 
     public void addScore(int id, int subject, int round, int score)
     {
-
     }
-
     public void updateScore(int id, int subject, int round, int score) {
     }
 
